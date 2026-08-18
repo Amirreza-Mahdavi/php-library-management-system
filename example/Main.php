1d35f8aeee79557ac382674b2221b457d903cdbd
Amirreza Mahdavi <amiirreza.mahdavii@gmail.com>
2026-08-18 13:16:56 +0330

    feat(example-Main): add program entry point

--- example/Main.php
+++ example/Main.php
@@ -1,1 +1,14 @@
 <?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+use LMS\CLI\Application;
+
+try {
+    $application = new Application();
+    $application->run();
+}
+catch (Throwable $e) {
+    fwrite(STDERR, "Error: {$e->getMessage()}" . PHP_EOL);
+    exit(1);
+}
